23ca7363205130ac94f7b3db8100b896c46c127d
rilfi <rilfimail@gmail.com>
2018-01-12 13:35:02 +0530

    asdad

--- src/main/java/com/stormadvance/storm_example/SampleBolt.java
+++ src/main/java/com/stormadvance/storm_example/SampleBolt.java
@@ -1,17 +1,42 @@
 package com.stormadvance.storm_example;
 
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseBasicBolt;
 import org.apache.storm.tuple.Tuple;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
 public class SampleBolt extends BaseBasicBolt {
 	private static final long serialVersionUID = 1L;
+	File myfile = new File("fl.txt");
+	FileWriter writer ;
+	public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+		try {
+			writer = new FileWriter(myfile, true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-	public void execute(Tuple input, BasicOutputCollector collector) {
+	}
+
+
+
+		public void execute(Tuple input, BasicOutputCollector collector) {
 		// fetched the field "site" from input tuple.
 		String test = input.getStringByField("site");
-		// print the value of field "site" on console.
+			try {
+				writer.write("Tomorrow will be cloudy.");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			// print the value of field "site" on console.
 		System.out.println("######### Name of input site is : " + test);
 	}
 
