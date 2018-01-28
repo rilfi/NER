6b4ba2012b6df7d5cbda5cd833aa34f3cc529fc7
rilfi <rilfimail@gmail.com>
2018-01-28 17:32:43 +0530

    asdadgjk

--- src/main/java/com/stormadvance/storm_example/SampleStormClusterTopology.java
+++ src/main/java/com/stormadvance/storm_example/SampleStormClusterTopology.java
@@ -2,10 +2,15 @@ package com.stormadvance.storm_example;
 
 import org.apache.storm.Config;
 import org.apache.storm.StormSubmitter;
+import org.apache.storm.bolt.JoinBolt;
 import org.apache.storm.generated.AlreadyAliveException;
 import org.apache.storm.generated.AuthorizationException;
 import org.apache.storm.generated.InvalidTopologyException;
 import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.topology.base.BaseWindowedBolt;
+import org.apache.storm.tuple.Fields;
+
+import java.util.concurrent.TimeUnit;
 
 public class SampleStormClusterTopology {
 	public static void main(String[] args) throws AlreadyAliveException,
@@ -13,14 +18,54 @@ public class SampleStormClusterTopology {
 		// create an instance of TopologyBuilder class
 		TopologyBuilder builder = new TopologyBuilder();
 		// set the spout class
-		builder.setSpout("TwitterSpout", new TwitterSpout("/root/tweets100.txt"), 2);
+		//builder.setSpout("TwitterSpout", new TwitterSpout("/root/tweets100.txt"), 2);
 		//builder.setBolt("CRFBolt",new CRFBolt("/root/brand_crf.model"),2).shuffleGrouping("TwitterSpout");
-        builder.setBolt("CRFBolt",new SentimentBolt("/root/brand_crf.model"),2).shuffleGrouping("TwitterSpout");
+       // builder.setBolt("CRFBolt",new SentimentBolt("/root/brand_crf.model"),2).shuffleGrouping("TwitterSpout");
 		// set the bolt class
 		/*builder.setBolt("SampleBolt", new SampleBolt(), 4).shuffleGrouping(
 				"TwitterSpout");*/
 		/*builder.setBolt("PersistenceBolt", new PersistenceBolt(args[0], args[1], args[2], args[3]), 1).shuffleGrouping(
 				"SampleBolt");*/
+
+		builder.setSpout("TwitterSpout", new TwitterSpout("/root/ptweets.txt"), 1);
+		builder.setBolt("brandNERBolt",new BrandNERBolt("/root/brand_crf.model"),4).shuffleGrouping("TwitterSpout");
+		builder.setBolt("productNERBolt",new ProductNERBolt("/root/product_crf.model"),4).shuffleGrouping("TwitterSpout");
+		builder.setBolt("GroupClassificationBolt",new GroupClassificationBolt("/root/group.model.LogReg"),4).shuffleGrouping("TwitterSpout");
+		builder.setBolt("StateClassificationBolt",new StateClassificationBolt("/root/status.model.LogReg"),4).shuffleGrouping("TwitterSpout");
+
+
+
+		JoinBolt nerJoiner = new JoinBolt("brandNERBolt", "id")
+				.join("productNERBolt",    "id","brandNERBolt")
+				.select ("id,brandset,productset")
+				.withTumblingWindow( new BaseWindowedBolt.Duration(10, TimeUnit.SECONDS) );
+		builder.setBolt("nerjoiner", nerJoiner)
+				.fieldsGrouping("brandNERBolt", new Fields("id"))
+				.fieldsGrouping("productNERBolt", new Fields("id"));
+
+		JoinBolt classifierJoiner = new JoinBolt("GroupClassificationBolt", "id")
+				.join("StateClassificationBolt",    "id","GroupClassificationBolt")
+				.select ("id,group,status")
+				.withTumblingWindow( new BaseWindowedBolt.Duration(10, TimeUnit.SECONDS) );
+		builder.setBolt("classifierJoiner", classifierJoiner)
+				.fieldsGrouping("GroupClassificationBolt", new Fields("id"))
+				.fieldsGrouping("StateClassificationBolt", new Fields("id"));
+
+		JoinBolt IEJoiner = new JoinBolt("nerjoiner", "id")
+				.join("classifierJoiner",    "id","nerjoiner")
+				.select ("id,brandset,productset,group,status")
+				.withTumblingWindow( new BaseWindowedBolt.Duration(10, TimeUnit.SECONDS) );
+		builder.setBolt("IEJoiner", IEJoiner)
+				.fieldsGrouping("nerjoiner", new Fields("id"))
+				.fieldsGrouping("classifierJoiner", new Fields("id"));
+
+		JoinBolt fainalJoiner = new JoinBolt("IEJoiner", "id")
+				.join("TwitterSpout",    "id","IEJoiner")
+				.select ("id,brandset,productset,group,status,tweet")
+				.withTumblingWindow( new BaseWindowedBolt.Duration(10, TimeUnit.SECONDS) );
+		builder.setBolt("fainalJoiner", fainalJoiner)
+				.fieldsGrouping("IEJoiner", new Fields("id"))
+				.fieldsGrouping("TwitterSpout", new Fields("id"));
 		Config conf = new Config();
 		conf.setNumWorkers(4);
 
