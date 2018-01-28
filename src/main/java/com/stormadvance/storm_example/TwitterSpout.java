9efbaa2ffa455c4f13d8a785d9b8c5491dc28e41
rilfi <rilfimail@gmail.com>
2018-01-28 17:21:43 +0530

    asdadgjk

--- src/main/java/com/stormadvance/storm_example/TwitterSpout.java
+++ src/main/java/com/stormadvance/storm_example/TwitterSpout.java
@@ -46,7 +46,7 @@ public class TwitterSpout extends BaseRichSpout {
             if((line=br.readLine())!=null) {
                 spoutOutputCollector.emit(new Values(id, line.toLowerCase()));
                 try{
-                    Thread.sleep(5000);
+                    Thread.sleep(50);
                 }catch(Exception e) {
                     System.out.println("Failed to sleep the thread");
                 }
