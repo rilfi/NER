51837b13ffca5886f65a21f91f5695c6f6be98db
rilfi <rilfimail@gmail.com>
2018-01-29 19:41:13 +0530

    asdadgjk

--- src/main/java/com/stormadvance/storm_example/SampleStormTopology.java
+++ src/main/java/com/stormadvance/storm_example/SampleStormTopology.java
@@ -41,25 +41,25 @@ public class SampleStormTopology {
 				.fieldsGrouping("GroupClassificationBolt", new Fields("id"))
 				.fieldsGrouping("StateClassificationBolt", new Fields("id"));
 
-		JoinBolt IEJoiner = new JoinBolt("nerjoiner", "id")
+		/*JoinBolt IEJoiner = new JoinBolt("nerjoiner", "id")
 				.join("classifierJoiner",    "id","nerjoiner")
 				.select ("id,brandset,productset,group,status")
 				.withTumblingWindow( new BaseWindowedBolt.Duration(10, TimeUnit.SECONDS) );
 		builder.setBolt("IEJoiner", IEJoiner)
 				.fieldsGrouping("nerjoiner", new Fields("id"))
-				.fieldsGrouping("classifierJoiner", new Fields("id"));
+				.fieldsGrouping("classifierJoiner", new Fields("id"));*/
 
-		JoinBolt fainalJoiner = new JoinBolt("IEJoiner", "id")
-				.join("brandNERBolt",    "id","IEJoiner")
-				.select ("id,brandset,productset,group,status,brandset1")
+		JoinBolt fainalJoiner = new JoinBolt("classifierJoiner", "id")
+				.join("TwitterSpout",    "id","classifierJoiner")
+				.select ("id,group,status,tweet")
 				.withTumblingWindow( new BaseWindowedBolt.Duration(10, TimeUnit.SECONDS) );
 		builder.setBolt("fainalJoiner", fainalJoiner)
-				.fieldsGrouping("IEJoiner", new Fields("id"))
-				.fieldsGrouping("brandNERBolt", new Fields("id"));
-		builder.setBolt("twitterBolt",new TwitterBolt(),1).shuffleGrouping("fainalJoiner");
+				.fieldsGrouping("classifierJoiner", new Fields("id"))
+				.fieldsGrouping("TwitterSpout", new Fields("id"));
+		//builder.setBolt("twitterBolt",new TwitterBolt(),1).shuffleGrouping("fainalJoiner");
 
 
-		builder.setBolt("printer", new PrinterBolt(),1 ).shuffleGrouping("twitterBolt");
+		builder.setBolt("printer", new PrinterBolt(),1 ).shuffleGrouping("fainalJoiner");
 
 		// set the bolt class
 		/*builder.setBolt("SampleBolt", new SampleBolt(), 4).shuffleGrouping(
